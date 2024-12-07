8949ca0f42de755acb06fdae64e1392bce7ed592
Frédéric Blanc <frederic.blanc@itanea.fr>
2024-12-07 22:58:07 +0100

    Update generated files

--- out/codelensProvider.js
+++ out/codelensProvider.js
@@ -2,11 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TestCodeLensProvider = void 0;
 exports.updateTestResult = updateTestResult;
+exports.getFailedTests = getFailedTests;
 const vscode = require("vscode");
-// Storage of test results
+// Store test results
 const testResults = new Map();
 function updateTestResult(filePath, testName, success) {
     testResults.set(`${filePath}:${testName}`, success);
+    console.log('testResults >>>');
+    console.log(testResults);
+    console.log('<<< End testResults');
+}
+function getFailedTests(filePath) {
+    const failedTests = [];
+    testResults.forEach((success, key) => {
+        if (!success && key.startsWith(filePath)) {
+            const testName = key.split(':')[1];
+            failedTests.push(testName);
+        }
+    });
+    return failedTests;
 }
 class TestCodeLensProvider {
     constructor() {
@@ -34,7 +48,7 @@ class TestCodeLensProvider {
                 const result = testResults.get(`${document.uri.fsPath}:${testName}`);
                 const title = result === true ? '✅ Run This Test' : result === false ? '❌ Run This Test' : 'Run This Test';
                 console.log(`Found test: ${testName} at line ${index + 1}`);
-                // Add CodeLens for “Run This Test
+                // Add CodeLens for "Run This Test"
                 codeLenses.push(new vscode.CodeLens(range, {
                     title,
                     command: 'pest.runSingleTest',
@@ -42,12 +56,21 @@ class TestCodeLensProvider {
                 }));
             }
         });
-        // Add a global CodeLens for “Run All Tests”.
+        // Add a global CodeLens for "Run All Tests"
         codeLenses.push(new vscode.CodeLens(new vscode.Range(0, 0, 0, 0), {
             title: 'Run All Tests in File',
             command: 'pest.runAllTests',
             arguments: [document.uri.fsPath],
         }));
+        // Add a global CodeLens for "Run Failed Tests" if tests failed
+        const failedTests = getFailedTests(document.uri.fsPath);
+        if (failedTests.length > 0) {
+            codeLenses.push(new vscode.CodeLens(new vscode.Range(0, 0, 1, 0), {
+                title: 'Rerun Failed Tests',
+                command: 'pest.runFailedTests',
+                arguments: [document.uri.fsPath],
+            }));
+        }
         console.log(`CodeLens added for document: ${document.fileName}`);
         return codeLenses;
     }
